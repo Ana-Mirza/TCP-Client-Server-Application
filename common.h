501788b91f4829ff6645b0f429e4afa92cb4b77c
ana <anamirza2002@gmail.com>
2023-05-06 14:12:09 +0300

    added structures for clients

--- common.h
+++ common.h
@@ -7,12 +7,30 @@
 int send_all(int sockfd, void *buff, size_t len);
 int recv_all(int sockfd, void *buff, size_t len);
 
-/* Dimensiunea maxima a mesajului */
+/* MAximum length of message */
 #define MSG_MAXSIZE 1024
+#define MAX_NAME 100
 
+/* strcture for a message received/sent */
 struct chat_packet {
   uint16_t len;
   char message[MSG_MAXSIZE + 1];
+};
+
+/* structure of a topic */
+struct topic {
+  char name[MAX_NAME];
+  int sf;
+};
+
+
+/* structure for a client */
+struct struct_client {
+  int fd;
+  char id[MAX_NAME];
+  int isConnected;
+  struct topic *topics_subscribed[MAX_NAME];
+  struct chat_packet *messages_received;
 };
 
 #endif
